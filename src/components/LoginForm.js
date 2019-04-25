164b0e7be97b68d10217c6f876a812653fdd54e9
github username <github email address>
2019-04-25 18:07:06 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,42 @@
 import React from "react";
 
 class LoginForm extends React.Component {
+
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  loginConfirm = (e) => {
+    e.preventDefault()
+    if (this.state.username && this.state.password) {
+      this.props.handleLogin(e)
+    }
+  }
+
+  handleOnChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.loginConfirm}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleOnChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleOnChange} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,28 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: '',
+      maxChars: 0
+    };
+  }
+
+
+
+  handleOnChange = (e) => {
+    this.setState({
+      message: e.target.value,
+      maxChars: this.props.maxChars
+    })
   }
 
   render() {
+    console.log(this.state.message)
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" value={this.state.message} onChange={this.handleOnChange} id="message" />
+        {this.props.maxChars - this.state.message.length}
       </div>
     );
   }
